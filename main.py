e7a79e95641f68f4d6749457468eefe09fdb9849
Antoine Van Hoye <antoine.vanhoye@student.uliege.be>
2024-10-19 11:31:14 +0200

    Starting point

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Group member:
+- Leo Di Puma           - Louis Belboom
+- Emile de Lamalle      - Diego Schyns
+- Amos David            - Antoine Van Hoye
+- Guillaume Vaillot
+"""
+
+import numpy as np
+import matplotlib as plt
+
